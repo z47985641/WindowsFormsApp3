82089326d9ca0b4c747736ef9588ad6283699bba
z47985641 <z47985641@gmail.com>
2022-07-11 09:23:49 +0800

    加入專案檔案。

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+
+   
+
+    public partial class Form1 : Form
+    {
+        int cmt = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmt += 1;
+            label1.Text = "次數" + cmt;
+
+        }
+    }
+}
